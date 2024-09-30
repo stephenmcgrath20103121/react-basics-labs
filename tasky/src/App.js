7f6796b7ebbbae19f6f355e1ac7fc5fab629ed66
Stephen McGrath <20103121@mail.wit.ie>
2024-09-30 12:48:33 +0100

    Delete a task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -18,6 +18,12 @@ function App() {
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -29,6 +35,7 @@ function App() {
             key={task.id}
             done={task.done}
             markDone={() => doneHandler(index)}
+            deleteTask = {() => deleteHandler(index)}
           />
         ))} 
     </div>
